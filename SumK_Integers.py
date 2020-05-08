c7af0af9e7d88896505778f837611a64845509b9
avinash273 <avinash.shanker@mavs.uta.edu>
2020-05-08 02:35:35 -0500

    find the maximum sum of K consecutive elements, Brute Force Sliding Window added

--- SumK_Integers.py
+++ SumK_Integers.py
@@ -3,19 +3,35 @@ Given an array of of N  integers, find the maximum sum of K consecutive elements
 """
 import sys
 
-maxVal = - sys.maxsize - 1
 
-def maxSuBruteForce(arr, k):
+def maxSumBruteForce(arr, k):
+    arrLen = len(arr)
+    maxSum = - sys.maxsize - 1
+    for i in range(0, arrLen - k + 1):
+        curSum = 0
+        for j in range(k):
+            curSum += arr[i + j]
+        maxSum = max(curSum, maxSum)
+    return maxSum
+
+
+def maxSumSlidingWindow(arr, windowSize):
+    arrLen = len(arr)
     maxSum = -sys.maxsize - 1
 
-    arrLen = len(arr)
-    for i in range(0, arrLen - k + 1):
-        currentSum = 0
-        for j in range(k):
-            currentSum += arr[i + j]
-        maxSum = max(currentSum, maxSum)
+    if (arrLen < windowSize):
+        return None
+    elif (arrLen == windowSize):
+        fullSum = sum(arr[i] for i in range(arrLen))
+        return fullSum
+
+    windowSum = sum(arr[i] for i in range(windowSize))
+    for i in range(0, arrLen - windowSize):
+        windowSum = windowSum - arr[i] + arr[i + windowSize]
+        maxSum = max(windowSum,maxSum)
     return maxSum
 
 
 arr = [80, -50, 90, 100]
-print(maxSuBruteForce(arr,3))
+print(maxSumBruteForce(arr, 2))
+print(maxSumSlidingWindow(arr, 2))
