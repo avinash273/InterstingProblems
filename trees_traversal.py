9c2dec3c052be78b5005c6d8582ab213daae487c
avinash273 <avinash.shanker@mavs.uta.edu>
2020-05-10 19:09:32 -0500

    Tree all three traversals

---
+++ trees_traversal.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self,value):
+        self.left = None
+        self.right = None
+        self.data = value
+
+def inorder(node):
+    if(node is not None):
+        inorder(node.left)
+        print(node.data)
+        inorder(node.right)
+
+def preorder(node):
+    if(node is not None):
+        print(node.data)
+        preorder(node.left)
+        preorder(node.right)
+
+def postorder(node):
+    if(node is not None):
+        postorder(node.left)
+        postorder(node.right)
+        print(node.data)
+
+root = Node(4)
+root.left = Node(5)
+root.right = Node(6)
+root.left.left = Node(7)
+
+preorder(root)
+inorder(root)
+postorder(root)
