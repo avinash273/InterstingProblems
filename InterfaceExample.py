4f9cd43318fd9d14babdfffbe9e2751201b00757
avinash273 <avinash.shanker@mavs.uta.edu>
2020-05-11 07:33:39 -0500

    Abstract class implementation, inheritance, overriding

--- InterfaceExample.py
+++ InterfaceExample.py
@@ -2,11 +2,9 @@
 Abstract class implementation
 """
 
-
 class AdvArithmetic(object):
     def divisorSum(self, n):
         raise NotImplementedError
-
 
 class Calculator(AdvArithmetic):
     def divisorSum(self, n):
@@ -19,3 +17,26 @@ class Calculator(AdvArithmetic):
 
 my_calculator = Calculator()
 print(my_calculator.divisorSum(6))
+
+"""
+Trying inheritance and others
+"""
+
+class A:
+    static_variable = 0
+    def __init__(self):
+        print("Init A")
+
+    def feature1(self):
+        print("Feature1")
+
+class B:
+    def feature3(self):
+        print("Feature3")
+
+class C(A, B):
+    def feature5(self):
+        print("Feature5")
+
+a1 = A()
+c1 = C()
